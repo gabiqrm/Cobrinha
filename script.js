55423da7c8a0d832d8dc67bdf3563a6c099baef6
Gabriela Queiroz <gabiqrm12@gmail.com>
2020-05-07 00:13:14 -0300

    Definir fim do jogo

--- script.js
+++ script.js
@@ -44,6 +44,13 @@ function iniciarJogo(){
     if(snake[0].y >15 * box && direction=="down") snake[0].y =0;
     if(snake[0].y <0  && direction=="up") snake[0].y = 16 * box;
 
+    for(i=1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y ==snake[i].y){
+            clearInterval(jogo);
+            alert('Gamer Over! :(');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
